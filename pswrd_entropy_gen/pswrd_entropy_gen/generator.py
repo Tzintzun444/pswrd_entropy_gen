b7ac74c1f60cd4b9a5c01f6abfa0e2243cdc573b
Tzintzun444 <alfredotzintzun4@gmail.com>
2025-04-11 23:28:30 -0600

    The function was renamed and was improved its functionality
    The old function called possible_characters was renamed as generate_password, also the dictionary of situations was modified
    for improve its performance. Finally the password generation was added with the use of the secrets module and improved.
    The function works for now.

--- pswrd_entropy_gen/pswrd_entropy_gen/generator.py
+++ pswrd_entropy_gen/pswrd_entropy_gen/generator.py
@@ -2,19 +2,31 @@ import secrets
 import string
 
 
-def possible_characters(use_uppercase=True, use_numbers=True, use_punctuations=True):
+def generate_password(lenght: int, use_uppercase=True,
+                      use_numbers=True, use_punctuations=True) -> str:
 
     characters = string.ascii_lowercase
-    situations = {use_uppercase: string.ascii_uppercase,
-                  use_numbers: string.digits,
-                  use_punctuations: string.punctuation}
+    situations = {'uppercase': (use_uppercase, string.ascii_uppercase),
+                  'numbers': (use_numbers, string.digits),
+                  'punctuations': (use_punctuations, string.punctuation)}
+    password = []
 
     for situation, character_type in situations.items():
 
-        if type:
+        if character_type[0]:
 
-            characters += character_type
+            characters += character_type[1]
+            password.append(secrets.choice(character_type[1]))
 
-    return characters
+    remaining = lenght - len(password)
+    random_password = [secrets.choice(characters) for _ in range(remaining)]
+    password.extend(random_password)
+
+    secrets.SystemRandom().shuffle(password)
+
+    final_password = ''.join(password)
+
+    return final_password
 
 
+print(generate_password(12))
