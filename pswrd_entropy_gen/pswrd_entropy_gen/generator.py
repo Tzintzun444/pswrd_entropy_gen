c0a5760eb9d67d976e76f64ff0d1d848a453e14e
Tzintzun444 <alfredotzintzun4@gmail.com>
2025-04-12 18:26:02 -0600

    The functions were transformed into a class called Generator, the constructor __init__ and the __str__ method were added and the class functions great!

--- pswrd_entropy_gen/pswrd_entropy_gen/generator.py
+++ pswrd_entropy_gen/pswrd_entropy_gen/generator.py
@@ -4,72 +4,97 @@ import string
 from typing import Union
 
 
-def generate_password(length: int, use_uppercase=True,
-                      use_numbers=True, use_punctuations=True) -> str:
+class Generator:
 
-    characters = string.ascii_lowercase
-    situations = {'uppercase': (use_uppercase, string.ascii_uppercase),
-                  'numbers': (use_numbers, string.digits),
-                  'punctuations': (use_punctuations, string.punctuation),
-                  }
-    password = []
+    def __init__(self, length):
 
-    for character_type in situations.values():
+        self._length = length
+        (self._generated_password, self._entropy_of_password,
+         self._decryption_password_time) = self.create_password()
 
-        if character_type[0]:
+    @property
+    def length(self):
+        return self._length
 
-            characters += character_type[1]
-            password.append(secrets.choice(character_type[1]))
+    @length.setter
+    def length(self, length):
+        self._length = length
 
-    remaining = length - len(password)
-    random_password = [secrets.choice(characters) for _ in range(remaining)]
-    password.extend(random_password)
+    @staticmethod
+    def generate_password(length: int, use_uppercase=True,
+                          use_numbers=True, use_punctuations=True) -> str:
 
-    secrets.SystemRandom().shuffle(password)
+        characters = string.ascii_lowercase
+        situations = {'uppercase': (use_uppercase, string.ascii_uppercase),
+                      'numbers': (use_numbers, string.digits),
+                      'punctuations': (use_punctuations, string.punctuation),
+                      }
+        password = []
 
-    final_password = ''.join(password)
+        for character_type in situations.values():
 
-    return final_password
+            if character_type[0]:
+
+                characters += character_type[1]
+                password.append(secrets.choice(character_type[1]))
+
+        remaining = length - len(password)
+        random_password = [secrets.choice(characters) for _ in range(remaining)]
+        password.extend(random_password)
+
+        secrets.SystemRandom().shuffle(password)
+
+        final_password = ''.join(password)
+
+        return final_password
+
+    @staticmethod
+    def calculate_entropy(password: str, decimals: int = 1) -> Union[int, float]:
+
+        unique_characters = set(password)
+        length_password = len(password)
+        argument_log = 0
+
+        situations = {'uppercase': (string.ascii_uppercase, 26),
+                      'lowercase': (string.ascii_lowercase, 26),
+                      'numbers': (string.digits, 10),
+                      'punctuations': (string.punctuation, 32),
+                      }
+
+        for character_type, posibilities in situations.values():
+
+            if any(character in character_type for character in unique_characters):
+
+                argument_log += posibilities
+
+        entropy = length_password * math.log2(argument_log) if length_password > 0 else 0
+
+        return round(entropy, decimals)
+
+    @staticmethod
+    def calculate_decryption_time(entropy: Union[int, float],
+                                  decimals: int = 2, attempts_per_second=1e12) -> Union[int, float]:
+
+        seconds_per_year = 60 * 60 * 24 * 365
+
+        combinations = 2 ** entropy
+        decryption_time_in_seconds = combinations / attempts_per_second
+        decryption_time_in_years = decryption_time_in_seconds / seconds_per_year
+
+        return round(decryption_time_in_years, decimals)
+
+    def create_password(self):
+
+        generated_password = self.generate_password(self.length)
+        entropy_of_password = self.calculate_entropy(generated_password)
+        decryption_password_time = self.calculate_decryption_time(entropy_of_password)
+
+        return generated_password, entropy_of_password, decryption_password_time
+
+    def __str__(self):
+        return (f'The password generated is {self._generated_password}, entropy={self._entropy_of_password} '
+                f'and the time necessary to decrypte it is {self._decryption_password_time} years')
 
 
-def calculate_entropy(password: str, decimals: int = 1) -> Union[int, float]:
-
-    unique_characters = set(password)
-    length_password = len(password)
-    argument_log = 0
-
-    situations = {'uppercase': (string.ascii_uppercase, 26),
-                  'lowercase': (string.ascii_lowercase, 26),
-                  'numbers': (string.digits, 10),
-                  'punctuations': (string.punctuation, 32),
-                  }
-
-    for character_type, posibilities in situations.values():
-
-        if any(character in character_type for character in unique_characters):
-
-            argument_log += posibilities
-
-    entropy = length_password * math.log2(argument_log) if length_password > 0 else 0
-
-    return round(entropy, decimals)
-
-
-def calculate_decryption_time(entropy: Union[int, float],
-                              decimals: int = 2, attempts_per_second=1e12) -> Union[int, float]:
-
-    seconds_per_year = 60 * 60 * 24 * 365
-
-    combinations = 2 ** entropy
-    decryption_time_in_seconds = combinations / attempts_per_second
-    decryption_time_in_years = decryption_time_in_seconds / seconds_per_year
-
-    return round(decryption_time_in_years, decimals)
-
-
-password = generate_password(12)
-entropy = calculate_entropy(password)
-decryption = calculate_decryption_time(entropy)
-print(password)
-print(entropy)
-print(decryption)
+generator = Generator(12)
+print(generator)
